20182469ce8c43d8c7f93441fbc562d6d3ce7d28
elmotec <1107551+elmotec@users.noreply.github.com>
2022-12-11 11:39:23 -0500

    feat: builds test.cpp against google benchmark

--- Makefile
+++ Makefile
@@ -1,2 +1,18 @@
-test: test.cpp
-	g++ -std=c++17 $< -o $@
+UID=$(shell id -u)
+GID=$(shell id -g)
+
+test: build
+	./.build/bmk
+
+configure: CMakeLists.txt
+	cmake -B.build .
+
+build:
+	(cd .build && make)
+
+docker: Dockerfile
+	docker build .
+
+shell:
+	docker compose run -u ${UID}:${GID} --rm -it dev
+
--- test.cpp
+++ test.cpp
@@ -1,105 +1,98 @@
-#include <unordered_map>
+#include "benchmark/benchmark.h"
 #include <map>
-#include <vector>
 #include <string>
-
+#include <unordered_map>
+#include <variant>
+#include <vector>
 
 using namespace std;
-struct Security
-{
-    string m_identifier;
-    map<string, double> m_data;
+
+constexpr string_view field0 = "field0";
+constexpr string_view field1 = "field1";
+constexpr string_view field2 = "field2";
+constexpr string_view field3 = "field3";
+constexpr string_view field4 = "field4";
+constexpr string_view field5 = "field5";
+constexpr string_view field6 = "field6";
+
+struct Security {
+  string m_identifier;
+  map<string, double> m_data;
 };
 
-Security makeSecurity()
-{
-    Security rv;
-    for (auto ii = 0; ii < 10; ii++)
-    {
-        rv.m_data.insert(make_pair("field"+to_string(ii), double()));
-    }
-    static int uuid = 0;
-    rv.m_identifier= to_string(++uuid);
-    return rv;
+Security makeSecurity() {
+  Security rv;
+  for (auto ii = 0; ii < 7; ii++) {
+    rv.m_data.insert(make_pair("field" + to_string(ii), double()));
+  }
+  static int uuid = 0;
+  rv.m_identifier = to_string(++uuid);
+  return rv;
 };
 
-struct StructPod
-{
-    double f0;
-    double f1;
-    double f2;
-    double f3;
-    double f4;
-    double f5;
-    double f6;
-    double f7;
-    double f8;
-    double f9;
+struct StructPod {
+  double f0;
+  double f1;
+  double f2;
+  double f3;
+  double f4;
+  bool f5;
+  bool f6;
 
-    StructPod(Security security)
-    {
-        f0 = security.m_data["field0"];
-        f1 = security.m_data["field1"];
-        f2 = security.m_data["field2"];
-        f3 = security.m_data["field3"];
-        f4 = security.m_data["field4"];
-        f5 = security.m_data["field5"];
-        f6 = security.m_data["field6"];
-        f7 = security.m_data["field7"];
-        f8 = security.m_data["field8"];
-        f9 = security.m_data["field9"];
+  StructPod(Security security) {
+    for (const auto &kv : security.m_data) {
+      if (kv.first == field0)
+        f0 = kv.second;
+      else if (kv.first == field1)
+        f1 = kv.second;
+      else if (kv.first == field2)
+        f2 = kv.second;
+      else if (kv.first == field3)
+        f3 = kv.second;
+      else if (kv.first == field4)
+        f4 = kv.second;
+      else if (kv.first == field5)
+        f5 = kv.second;
+      else if (kv.first == field6)
+        f6 = kv.second;
     }
+  }
 };
 
-struct MapLike
-{
-    map<string, double> m_internals;
-    MapLike(const Security & security)
-    {
-        for (const auto & kv : security.m_data) 
-        {
-            m_internals[kv.first] = kv.second;
-        }
+struct MapLike {
+  map<string, variant<double, bool, string>> m_internals;
+  MapLike(const Security &security) {
+    for (const auto &kv : security.m_data) {
+      m_internals[kv.first] = kv.second;
     }
+  }
 };
 
-template<typename T>
-void handle(const vector<Security> & allSecurities)
-{
-    unordered_map<string, T> container;
-    for (const auto & security : allSecurities)
-    {
-        T element(security);
-        container.insert(make_pair(security.m_identifier, element));
-    }
+template <typename T> void handle(const vector<Security> &allSecurities) {
+  unordered_map<string, T> container;
+  for (const auto &security : allSecurities) {
+    T element(security);
+    container.insert(make_pair(security.m_identifier, element));
+  }
 }
 
-//#define TEST
-#if defined(TEST)
-namespace benchmark
-{
-    using State = vector<int>;
-    template <typename T>
-        void DoNotOptimize(const T &);
-}
+const int nSecurities = 100;
+vector<Security> securities(nSecurities);
 
-#define BENCHMARK(ignored)
-#endif // TEST
-
-static void ParseToPod(benchmark::State& state) {
-    // Code inside this loop is measured repeatedly
-    vector<Security> securities(1000);
+static void ParseToPod(benchmark::State &state) {
+  for (auto _ : state) {
     handle<StructPod>(securities);
+  }
 }
 // Register the function as a benchmark
 BENCHMARK(ParseToPod);
 
-static void ParseToMapLike(benchmark::State& state) {
-    // Code before the loop is not measured
-    vector<Security> securities(1000);
+static void ParseToMapLike(benchmark::State &state) {
+  // Code before the loop is not measured
+  for (auto _ : state) {
     handle<MapLike>(securities);
+  }
 }
 BENCHMARK(ParseToMapLike);
 
-int main(int argc, const char * argv[])
-{ return 0; }
+BENCHMARK_MAIN();
